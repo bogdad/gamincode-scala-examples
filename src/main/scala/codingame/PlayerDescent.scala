ad3b4cd14c594c0ab96ad5a49bb54e8143f3d587
Vladimir Shakhov <bogdad@gmail.com>
2014-10-19 15:38:33 +0400

    compilation fix, readme, gitignore

--- src/main/scala/codingame/PlayerDescent.scala
+++ src/main/scala/codingame/PlayerDescent.scala
@@ -5,7 +5,7 @@ import scala.util._
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-object Player {
+object PlayerDescent {
 
   class Mnt(val sx:Int, val h:Int)
   
@@ -15,13 +15,13 @@ object Player {
       val Array(sx, sy) = for (i <- readLine split " ") yield i.toInt
 
       val rng: List[Int] = List.range(0, 7)
-      val mountains = rng.map((i) => new Mnt(i, readInt))
+      val mountains = rng.map((i) => new Mnt(i, readInt()))
 
       val highest = mountains.filter(mnt => mnt.sx >= sx).maxBy(_.h)
       // Write an action using println
       // To debug: Console.err.println("Debug messages...")
       // either:  FIRE (ship is firing its phase cannons) or HOLD (ship is not firing).
-      if (sx == highest) {
+      if (sx == highest.sx) {
         println("FIRE")
       } else {
         println("HOLD")
